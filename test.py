16f7f97953fe62ff88b79c903f029ced836cd595
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-19 11:08:42 -0600

    Fix function definition and calls

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from model import CimatOilSpillModel
 
 classes = CimatOilSpillDataset.CLASSES
 
-def test(input_dir, output_dir, arch, encoder, train_dataset, cross_dataset, test_dataset, num_epochs):
+def process(input_dir, output_dir, arch, encoder, train_dataset, cross_dataset, test_dataset, num_epochs):
     train_dataset, valid_dataset, test_dataset = create_datasets(input_dir, train_dataset, cross_dataset, test_dataset)
     train_dataloader, valid_dataloader, test_dataloader = create_dataloaders(os.cpu_count(), train_dataset, valid_dataset,
                                                                          test_dataset)
@@ -20,6 +20,8 @@ def test(input_dir, output_dir, arch, encoder, train_dataset, cross_dataset, tes
                          resume_from_checkpoint=f"{arch}_{encoder}_{num_epochs}epochs.ckpt")
     trainer.fit(model, train_dataloader=train_dataloader, val_dataloaders=valid_dataloader)
 
+def test(arch, encoder, input_dir, output_dir, train_dataset, cross_dataset, test_dataset, num_epochs):
+    process(input_dir, output_dir, arch, encoder, train_dataset, cross_dataset, test_dataset, num_epochs)
 
 parser = argparse.ArgumentParser(
     prog='Oil spill cimat dataset segmentation',
@@ -34,7 +36,6 @@ parser.add_argument('cross_dataset')
 parser.add_argument('test_dataset')
 parser.add_argument('num_epochs')
 args = parser.parse_args()
-print(args)
 arch = args.arch
 encoder = 'resnet34'
 test(arch, encoder, args.input_dir, args.output_dir, args.train_dataset, args.cross_dataset, args.test_dataset, int(args.num_epochs))
